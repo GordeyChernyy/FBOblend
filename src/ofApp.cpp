d89e24c5b8fb48a47e91b2e16c5ba3781d523c91
gordey <kewava@gmail.com>
2015-10-07 14:01:59 -0400

    First commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,115 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+//    ofDisableTextureEdgeHack();
+//    ofEnableTextureEdgeHack();
+
+    fbo1.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+    fbo1.begin();
+        ofClear(255, 0);
+    fbo1.end();
+    
+    fbo2.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+    fbo2.begin();
+        ofClear(255, 0);
+    fbo2.end();
+    ofBackground(255, 255, 255, 255);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofEnableAlphaBlending();
+    
+    ofSetColor(255,255);
+    fbo1.draw(0,0);
+    fbo2.draw(0,0);
+    
+    string s;
+    s.append("change layer: press SPACE\n");
+    s.append("layer: "+ofToString(layer));
+    ofSetColor(0,255);
+    ofDrawBitmapString(s, 20, 20);
+    
+    ofDisableAlphaBlending();
+}
+
+void ofApp::drawBrush(ofFbo &fbo){
+    ofVec2f m(mouseX, mouseY);
+    history.push_back(m);
+    for(int i=0; i< history.size(); i++){
+        ofVec2f h = history[i];
+        if (m.match(h, ofRandom(3, 200))){
+            fbo.begin();
+                ofEnableAlphaBlending();
+                    ofNoFill();
+                    ofSetColor(color, opacity);
+                    ofSetLineWidth(1.53);
+                    ofLine(h.x, h.y, m.x, m.y);
+                ofDisableAlphaBlending();
+            fbo.end();
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if (key ==' ') {
+        layer == 1? layer = 2: layer = 1;
+    }
+    if (key == '1') color = ofColor(255, 0);
+    if (key == '2') color = ofColor(0, 0);
+    if (key == '3') color = ofColor(255, 0, 0);
+    if (key == '4') color = ofColor(0, 255, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    switch (layer) {
+        case 1:
+            drawBrush(fbo1);
+            break;
+        case 2:
+            drawBrush(fbo2);
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    history.clear();
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+    void circle(ofColor c){
+        ofSetColor(c);
+        ofCircle(mouseX, mouseY, 20);
+    };
+		void update();
+		void draw();
+    
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+        void drawBrush(ofFbo &fbo);
+        int brush = 2;
+        int dist_points = 3;
+        int num_points = 3;
+        int point = 2;
+        bool drag = false;
+        int layer = 1;
+    
+    vector<ofVec2f> history;
+    ofColor color = ofColor(0);
+    int opacity = 50;
+    
+    ofFbo fbo1;
+    ofFbo fbo2;
+};
