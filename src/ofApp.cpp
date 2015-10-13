52c420884f2ad9e95fcf57c93611e5a0de30ae2e
steve lackermann <steve.lackermann@gmail.com>
2015-10-13 10:22:57 -0400

    first test commit

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,7 +4,7 @@
 void ofApp::setup(){
 //    ofDisableTextureEdgeHack();
 //    ofEnableTextureEdgeHack();
-
+    opacity = 100;
     fbo1.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
     fbo1.begin();
         ofClear(255, 0);
@@ -25,7 +25,7 @@ void ofApp::update(){
 void ofApp::draw(){
     ofEnableAlphaBlending();
     
-    ofSetColor(255,255);
+    ofSetColor(255,255,255,255);
     fbo1.draw(0,0);
     fbo2.draw(0,0);
     
@@ -45,15 +45,18 @@ void ofApp::draw(){
 }
 
 void ofApp::drawBrush(ofFbo &fbo){
+    static int counter =0;
+    counter +=1;
     ofVec2f m(mouseX, mouseY);
     history.push_back(m);
     for(int i=0; i< history.size(); i++){
         ofVec2f h = history[i];
         if (m.match(h, ofRandom(3, 200))){
+            cout << h.x << " " << h.y << " " << counter << "\n";
             fbo.begin();
                 ofEnableAlphaBlending();
-                    ofNoFill();
-                    ofSetColor(color, opacity);
+                    //ofNoFill();
+                    ofSetColor(color);//, opacity);
                     ofSetLineWidth(1.53);
                     ofLine(h.x, h.y, m.x, m.y);
                 ofDisableAlphaBlending();
@@ -64,13 +67,24 @@ void ofApp::drawBrush(ofFbo &fbo){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-    if (key ==' ') {
-        layer == 1? layer = 2: layer = 1;
+    switch(key) {
+        case ' ' :
+            1 == layer ? layer = 2 : layer = 1;
+            break;
+        case '1' :
+            color = ofColor(255, 0, 0, 32);
+            break;
+        case '2' :
+            color = ofColor(255, 0, 0, 64);
+            break;
+        case '3' :
+            color = ofColor(255, 0, 0, 128);
+            break;
+        case '4' :
+            color = ofColor(255, 0, 0, 255);
+            break;
+            
     }
-    if (key == '1') color = ofColor(255, 0);
-    if (key == '2') color = ofColor(0, 0);
-    if (key == '3') color = ofColor(255, 0, 0);
-    if (key == '4') color = ofColor(0, 255, 0);
 }
 
 //--------------------------------------------------------------
